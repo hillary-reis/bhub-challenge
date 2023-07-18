d44dbfad112bfbfb7d78954ba0c39dccc1d2105d
Hillary Reis <hillary.reis2002@gmail.com>
2023-07-18 12:31:11 -0300

    feat: adding endpoint to delete by uuid a customer

--- src/bhub/endpoints/customers/controllers.py
+++ src/bhub/endpoints/customers/controllers.py
@@ -96,5 +96,34 @@ async def customers_update_by_uuid(
         logger.exception(f'[{tracking_id}] Error while update customer: {exception.args}')
         return JSONResponse(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
                             content={'title': 'Internal error', 'detail': 'Error while update customer'})
+
+
+@router.delete('/v1/customers/{customer_uuid}', tags=['customers'], status_code=status.HTTP_200_OK)
+@inject
+async def customers_delete_by_uuid(
+    request: Request, customer_uuid: str,
+    customers_delete_by_uuid_use_case: CustomersDeleteByUuidUseCase = Depends(Provide[
+        Container.customers_delete_by_uuid_use_case]),
+    logger: Logger = Depends(Provide[Container.logger])
+) -> dict:
+    try:
+        tracking_id = request.headers.get('requestId', str(uuid4()))
+        logger.info(f'[{tracking_id}] starting get customer {customer_uuid}')
+
+        await customers_delete_by_uuid_use_case.run(tracking_id=tracking_id, customer_uuid=customer_uuid)
+
+        return JSONResponse(status_code=status.HTTP_200_OK, content={'message': 'Customer successfully deleted'})
+
+    except CustomerNotFoundException as exception:
+        logger.exception(f'[{tracking_id}] Customer not found: {exception.args[0]}')
+        return JSONResponse(status_code=status.HTTP_404_NOT_FOUND,
+                            content={'title': exception.args[0]['title'], 'detail': exception.args[0]['detail']})
+
+    except Exception as exception:
+        logger.exception(f'[{tracking_id}] Error while delete customer: {exception.args}')
+        return JSONResponse(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+                            content={'title': 'Internal error', 'detail': 'Error while delete customer'})
+
+
 def configure(app: FastAPI):
     app.include_router(router)
