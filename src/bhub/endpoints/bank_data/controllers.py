7c65af7920f5cdfdd39a3937490b8d4176247434
Hillary Reis <hillary.reis2002@gmail.com>
2023-07-18 12:05:20 -0300

    feat: adding endpoint to update a bank data by uuid

--- src/bhub/endpoints/bank_data/controllers.py
+++ src/bhub/endpoints/bank_data/controllers.py
@@ -76,5 +76,34 @@ async def bank_data_get_by_uuid(
         return JSONResponse(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
                             content={'title': 'Internal error', 'detail': 'Error while get bank data'})
 
+
+@router.patch('/v1/bank-data/{bank_data_uuid}', tags=['bank-data'], status_code=status.HTTP_200_OK)
+@inject
+async def bank_data_update_by_uuid(
+    request: Request, bank_data_uuid: str, schema: BankDataUpdatePayload,
+    bank_data_update_by_uuid_use_case: BankDataUpdateByUuidUseCase = Depends(Provide[
+        Container.bank_data_update_by_uuid_use_case]),
+    logger: Logger = Depends(Provide[Container.logger])
+) -> dict:
+    try:
+        tracking_id = request.headers.get('requestId', str(uuid4()))
+        logger.info(f'[{tracking_id}] starting get bank data {bank_data_uuid}')
+
+        await bank_data_update_by_uuid_use_case.run(tracking_id=tracking_id, bank_data_uuid=bank_data_uuid,
+                                                    schema=schema)
+
+        return JSONResponse(status_code=status.HTTP_200_OK, content={'message': 'Bank data successfully updated'})
+
+    except BankDataNotFoundException as exception:
+        logger.exception(f'[{tracking_id}] Bank data not found: {exception.args[0]}')
+        return JSONResponse(status_code=status.HTTP_404_NOT_FOUND,
+                            content={'title': exception.args[0]['title'], 'detail': exception.args[0]['detail']})
+
+    except Exception as exception:
+        logger.exception(f'[{tracking_id}] Error while update bank data: {exception.args}')
+        return JSONResponse(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+                            content={'title': 'Internal error', 'detail': 'Error while update bank data'})
+
+
 def configure(app: FastAPI):
     app.include_router(router)
